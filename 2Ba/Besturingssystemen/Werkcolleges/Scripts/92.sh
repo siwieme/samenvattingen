3a3790679fc3f702f1087effb51350a50dc4693b
Sibren Wieme <sibren.wieme@ugent.be>
2024-03-26 13:02:52 +0100

    2Ba - Besturingssystemen - Werkcolleges - 26/03/2024

---
+++ 2Ba/Besturingssystemen/Werkcolleges/Scripts/92.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+read n naam < <(wc -l $1)
+
+echo $n
+
+index=$((n-$2))
+teller=0
+
+while read lijn
+do 
+    if (($teller >= $index))
+    then
+        echo $lijn
+    fi
+    ((teller++))
+done < "$1"
